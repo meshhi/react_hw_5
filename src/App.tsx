749aa59cf6e42598f329b3ba0d85dd9d97ab44b1
Michael <yumythug@yandex.ru>
2023-11-07 20:13:48 +0300

    crud

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
 import './App.css'
 import { WorldClock } from './components/WorldClock/WorldClock'
+import { Routes, Route, Link } from 'react-router-dom';
+import Crud from './components/CRUD/Crud';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <WorldClock></WorldClock>
+    <div style={{display: "flex", gap: "1rem", justifyContent: "center", alignItems: "center", marginTop: "1rem"}}>
+      <Link to="/world-clock">WorldClock</Link>
+      <Link to="/crud">Crud</Link>
+    </div>
+      <Routes>
+        <Route path="/world-clock" element={<WorldClock></WorldClock>}></Route>
+        <Route path="/crud" element={<Crud></Crud>}></Route>
+      </Routes>
     </>
   )
 }
